9af798c66ba3cc4c003861127371946a18ac2fa8
Spandan <91448111+Warmachine019@users.noreply.github.com>
2024-12-21 23:23:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ for row in sheet_data:
 
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
-
 customer_data = data_manager.get_customer_emails()
 customer_email_list = [row["whatIsYourEmail?"] for row in customer_data]
 
